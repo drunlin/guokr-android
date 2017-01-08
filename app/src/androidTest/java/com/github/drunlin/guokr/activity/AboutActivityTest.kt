a9fab9e9ab257050498d67ffdf451b21bbac6673
drunlin <drunlin@outlook.com>
2017-01-08 21:36:27 +0800

    配置 Kotlin

---
+++ app/src/androidTest/java/com/github/drunlin/guokr/activity/AboutActivityTest.kt
@@ -0,0 +1,13 @@
+package com.github.drunlin.guokr.activity
+
+import com.github.drunlin.guokr.test.ActivityTestCase
+
+/**
+ * @author drunlin@outlook.com
+ */
+class AboutActivityTest : ActivityTestCase<AboutActivity>(AboutActivity::class.java) {
+
+    @Throws(Throwable::class)
+    override fun onPreview() {
+    }
+}
